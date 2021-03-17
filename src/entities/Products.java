bab16c69ae11442be29f258246d2519d7b74a07b
Denise Mello <denisemello.zero@gmail.com>
2021-03-16 22:20:03 -0300

    Exercicio arquivo criado

---
+++ src/entities/Products.java
@@ -0,0 +1,49 @@
+package entities;
+
+public class Products {
+	
+	String nome;
+	double preço;
+	int quantidade;
+	
+	public Products() {
+		
+	}
+	
+	public Products(String nome, double preço, int quantidade) {
+		this.nome = nome;
+		this.preço = preço;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(double preço) {
+		this.preço = preço;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	public double total() {
+		return preço*quantidade;
+	}
+	
+	
+
+}
